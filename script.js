00a209fb561fb74a20647b9e3f35eb20647ec1e9
neginhsobhani <neginhsobhani@gmail.com>
2021-12-03 03:45:55 +0330

    CSS styling improvement

--- script.js
+++ script.js
@@ -4,27 +4,46 @@ const probabilityPrediction = document.getElementById('probability-prediction');
 const maleRadioButton = document.getElementById('male-radio-button');
 const femaleRadioButton = document.getElementById('female-radio-button');
 const savedGender = document.getElementById('saved-gender');
+const clearButton = document.getElementById('clear-button');
+const savedAnswerHeader = document.getElementById('saved-answer-header');
+const popUP = document.getElementById('pop-up');
+
 
 function getData(){
     if(/^[a-zA-Z ]{1,255}$/.test(inputName.value)){
         fetch('https://api.genderize.io/?name='+inputName.value)
         .then(result => result.json()) 
         .then(result => {
+            if(!result.gender){
+                popUP.style.visibility = 'visible';
+                setTimeout(() => {
+                    popUP.style.visibility = 'hidden';
+                },3000)
+            }
             console.log(result);
             genderPrediction.innerHTML = 'gender: ' + result.gender;
             probabilityPrediction.innerHTML = 'probability: ' + result.probability;
             if(localStorage.getItem(inputName.value)){
+                savedAnswerHeader.style.visibility = 'visible';
+                clearButton.style.visibility = 'visible';
+                savedGender.style.visibility = 'visible';
                 savedGender.innerHTML = localStorage.getItem(inputName.value);
             }
             else{
-                savedGender.innerHTML = 'name not saved';
+                savedAnswerHeader.style.visibility = 'hidden';
+                clearButton.style.visibility = 'hidden';
+                savedGender.style.visibility = 'hidden';
+                savedGender.innerHTML = 'name not saved'; 
             }
         })
     }
     else{
-        alert('Wrong input format!')
+        popUP.style.visibility = 'visible';
+        setTimeout(() => {
+            popUP.style.visibility = 'hidden';
+        },3000)
     }
-        
+        event.preventDefault();   
 }
 
 function savePrediction(){
@@ -41,4 +60,7 @@ function savePrediction(){
 
 function clearSavedPrediction(){
     localStorage.removeItem(inputName.value);
+    savedAnswerHeader.style.visibility = 'hidden';
+    clearButton.style.visibility = 'hidden';
+    savedGender.style.visibility = 'hidden';
 }
